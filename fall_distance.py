311b7a48455274a0f29e3c6f6c7a1b220c3a6775
Conor <conor.okeefe.2121@gmail.com>
2021-04-21 19:08:53 -0700

    Calculates distance traveled by free fall for a given time

--- fall_distance.py
+++ fall_distance.py
@@ -2,6 +2,6 @@
 # Date: 4/21/21
 # Description: Calculates distance traveled by free fall for given time
 
-def dist(num):
+def fall_distance(num):
     """Applies formula for gravity to a number num using floats"""
     return(float(.5 * 9.8 * num ** 2))
